05d33101fc19f8684211f67fbd189a49296d9915
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-08 15:02:57 +0530

    preprocessing pipeline of videos finished

--- eye-pipeline/preprocessing.py
+++ eye-pipeline/preprocessing.py
@@ -1,6 +1,7 @@
 from vid_prepper import metadata
 from vid_prepper import standardize
 from vid_prepper import loader
+from vid_prepper import augmentor
 
 def extract_metadata(video_path):
     video_info = metadata.Metadata.validate_videos([video_path])[0]
@@ -29,3 +30,24 @@ def load_tensors(video_path: str):
 
     video_tensor = video_loader.load_file(video_path)
     return video_tensor
+
+
+def augment_video(video_tensor):
+    video_augmentor = augmentor.VideoAugmentor(
+        device="cpu",
+        use_gpu=False
+    )
+
+    cropped = augmentor.crop(video_tensor, type="center", size=(200, 200))
+    flipped = augmentor.flip(video_tensor, type="horizontal")
+    brightened = augmentor.brightness(video_tensor, amount=0.2)
+
+    augmentations = [
+            ('crop', {'type': 'random', 'size': (180, 180)}),
+            ('flip', {'type': 'horizontal'}),
+            ('brightness', {'amount': 0.1}),
+            ('contrast', {'amount': 0.1})
+        ]
+    chained_result = augmentor.chain(video_tensor, augmentations)
+    return chained_result
+    
