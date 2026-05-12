7206e057afcffcbe9c7fa1dd00efec72565d62b4
Samiksha Agarwal <agarwalsamiksha88@gmail.com>
2026-05-12 21:02:04 +0530

    Removed average functionality

--- backend/models/Result.go
+++ backend/models/Result.go
@@ -4,10 +4,8 @@ import "time"
 
 type Result struct {
 	ID         int       `json:"id"`
-	SNO        int       `json:"sno"`
 	UserId     int       `json:"userid"`
 	Probabilty float64   `json:"probability"`
-	Average    float64   `json:"average"`
 	ScreenTime float64   `json:"screentime"`
 	Time       time.Time `json:"-"`
 }
